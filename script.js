3e177a36e93e1e649d758f6f1ac366d1c6c6b9a5
sumermarano <sumermarano@gmail.com>
2023-02-15 18:25:19 +0200

    Add game function

--- script.js
+++ script.js
@@ -12,10 +12,33 @@ function playRound(com, usr) {
     if (usr === com) {
         return console.log("It's a tie!");
     } else if (usr === "rock" && com === "scissors" || usr === "paper" && com === "rock" || usr === "scissors" && com === "paper") {
-        return console.log(`You Win! ${usr} beats ${com}`); 
+        console.log(`You Win! ${usr} beats ${com}`);
+        return 'usr'; 
     } else {
-        return console.log(`You Lose! ${com} beats ${usr}`);
+        console.log(`You Lose! ${com} beats ${usr}`);
+        return 'com'; 
     }
 }
 
-playRound(computerSelection(), userSelection());
+function game() {
+    let usrScore = 0;
+    let comScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let round = playRound(computerSelection(), userSelection());
+        if (round === 'usr') {
+            usrScore++;
+        } else if (round === 'com') {
+            comScore++;
+        }
+    }
+
+    if (usrScore > comScore) {
+        console.log("You Won!!!");
+    } else if (usrScore < comScore) {
+        console.log("Oh No! You Lost.");
+    } else {
+        console.log("It's a Draw!");
+    }
+}
+
+game();
